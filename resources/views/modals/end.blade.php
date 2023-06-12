13a00c368464515b8ead02fada0aa32ec6f74ae6
etnik-dua <etnik.beqiri@dua.com>
2023-06-12 16:57:32 +0200

    started at fix

--- resources/views/modals/end.blade.php
+++ resources/views/modals/end.blade.php
@@ -73,7 +73,7 @@
                                 </div>
 
                                 <h1 class="text-2xl font-extralight">Started at
-                                    - {{$item->session->started_at->format('H:m:s - D.d')}}</h1>
+                                    - {{$item->session->started_at}}</h1>
                                 <hr class="w-80 h-1 mx-auto my-3 bg-gray-100 border-0 rounded">
                                 <h1 class="text-2xl font-extralight">Active For
                                     - <span
