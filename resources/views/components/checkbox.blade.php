0885ffd2870dfbb894d541fd91d540e78e8c36ac
etnik-dua <etnik.beqiri@dua.com>
2023-06-17 13:11:04 +0200

    auth page changes

--- resources/views/components/checkbox.blade.php
+++ resources/views/components/checkbox.blade.php
@@ -1,1 +1,1 @@
-<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500']) !!}>
+<input type="checkbox" {!! $attributes->merge(['class' => 'rounded border-orange-500 text-orange-500 shadow-sm focus:ring-orange-500']) !!}>
