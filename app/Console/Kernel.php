724eccba96803b9b82761970cd796fb42e6b0dc7
etnik-dua <etnik.beqiri@dua.com>
2023-06-17 12:43:28 +0200

    database backup

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('backup:database')->everyMinute();
+        $schedule->command('backup:database')->everyTwoHours();
 
     }
 
