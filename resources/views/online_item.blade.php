3aac38f0823f47d637e5cc9ca8b44441c807365e
etnik-dua <etnik.beqiri@dua.com>
2023-06-20 18:46:35 +0200

    if order add icon to display on online item

--- resources/views/online_item.blade.php
+++ resources/views/online_item.blade.php
@@ -26,10 +26,16 @@
                     <div>
                         <p class="font-bold text-4xl"> {{number_format($displayPrice, 2, ".", ".")}}€</p>
                         <p class="flex justify-end font-extralight text-xl">{{ $item->session->started_at->diffForHumans() }}</p>
+                        <p class="flex justify-end p-1">
+                            @if($item->session->products->count() > 0)
+                                <svg class="text-red-400 opacity-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M18.06 23h1.66c.84 0 1.53-.65 1.63-1.47L23 5.05h-5V1h-1.97v4.05h-4.97l.3 2.34c1.71.47 3.31 1.32 4.27 2.26c1.44 1.42 2.43 2.89 2.43 5.29V23M1 22v-1h15.03v1c0 .54-.45 1-1.03 1H2c-.55 0-1-.46-1-1m15.03-7C16.03 7 1 7 1 15h15.03M1 17h15v2H1v-2Z"/></svg>
+                            @endif
+                        </p>
                     </div>
                 </div>
             </div>
         </div>
+
         <div>
             <div class="flex items-center justify-between mb-2">
                 <h1 class="text-3xl font-bold leading-none text-gray-900">{{ $item->name }}</h1>
